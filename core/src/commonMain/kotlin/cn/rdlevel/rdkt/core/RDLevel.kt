c0aadc39e5bdb35549d5dad5c3c740fb1734eed9
NoMathExpectation <85624722+NoMathExpectation@users.noreply.github.com>
2025-06-24 14:35:56 +0800

    Added kdoc for RDLevel.

--- core/src/commonMain/kotlin/cn/rdlevel/rdkt/core/RDLevel.kt
+++ core/src/commonMain/kotlin/cn/rdlevel/rdkt/core/RDLevel.kt
@@ -3,6 +3,9 @@ package cn.rdlevel.rdkt.core
 import cn.rdlevel.rdkt.core.settings.LevelSettings
 import kotlinx.serialization.Serializable
 
+/**
+ * A Rhythm Doctor custom level representation.
+ */
 @Serializable
 public data class RDLevel(
     public val settings: LevelSettings,
